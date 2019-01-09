95912fa6bd537c5319ba7677e3d3921968647b2a
kuroikyu <kuroikyu+github@gmail.com>
2019-01-09 08:35:29 +0000

    Updates

---
+++ precache-manifest.c8a103d1f2a39b4a42a3b06ad7d20564.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4107770d67490c8bbc23",
+    "url": "/spacex-launches/static/css/main.5a689ae6.chunk.css"
+  },
+  {
+    "revision": "4107770d67490c8bbc23",
+    "url": "/spacex-launches/static/js/main.4107770d.chunk.js"
+  },
+  {
+    "revision": "c287a2d0e8d81b81b88e",
+    "url": "/spacex-launches/static/css/1.100a7594.chunk.css"
+  },
+  {
+    "revision": "c287a2d0e8d81b81b88e",
+    "url": "/spacex-launches/static/js/1.c287a2d0.chunk.js"
+  },
+  {
+    "revision": "ac122b9777b3f416e5d3",
+    "url": "/spacex-launches/static/js/runtime~main.ac122b97.js"
+  },
+  {
+    "revision": "ed7e527a87cccbcac71cdff97ece5803",
+    "url": "/spacex-launches/static/media/temp-badge.ed7e527a.png"
+  },
+  {
+    "revision": "06721f0ee477dfd125edba1228922b4f",
+    "url": "/spacex-launches/index.html"
+  }
+];
